d4f274bd64dbb86582922282d1f11a72703f8762
JamalGuyo <jamalguyo08@gmail.com>
2023-03-12 16:10:10 +0300

    setup jasmine and add install rimraf to delete dist folder on each build

---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,10 @@
+
+describe("Testing test", () => {
+    it("should expect 3 to equal 3", () => {
+        expect(3).toEqual(3);
+    })
+
+    it("should expect strings to match", () => {
+        expect("jamal").toBe("jamal")
+    })
+})
