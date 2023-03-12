af0f2bb2806bc076552e2f9473f57d5ae08c91a5
JamalGuyo <jamalguyo08@gmail.com>
2023-03-12 15:41:44 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to the Image Processing API project</h1>')
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}...`))
